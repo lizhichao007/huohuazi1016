5bdc8713e9338ee9537c1774d4325bc9250c44b3
lizhichao007 <15619303358@163.com>
2025-03-07 09:57:43 +0800

    fix

--- scripts/circle.py
+++
@@ -1,8 +0,0 @@
-from manim import *
-class MyScene(Scene):
-    def construct(self):
-        text = Text("Create Circle",color=WHITE)
-        self.play(FadeIn(text))
-        circle=Circle(radius=2,color=WHITE)
-        self.play(Create(circle))
-        self.wait(1)
