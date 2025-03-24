40e2b76c8205d8f429ac2e00543a17f9dc8733b9
lizhichao007 <15619303358@163.com>
2025-03-24 09:04:11 +0800

    Update python.py

--- Python/Object-oriented/python.py
+++ Python/Object-oriented/python.py
@@ -61,12 +61,61 @@
 # print(Test_D.new_name)
 
 # 类的实例化
-class Class_E(object):
-    name = 'huohuazi1016'
-    @classmethod
-    def fun_test(self):
-        print('name:' + self.name)
+# class Class_E(object):
+#     name = 'huohuazi1016'
+#     def fun_test(self):
+#         print('name:' + self.name)
 
-A = Class_E()
-print(A.name)
-A.fun_test()
+# A = Class_E()
+# print(A.name)
+# A.fun_test()
+
+# 一个类可以创造多个实例
+# class Class_F(object):
+#     name = 'huohuazi1016'
+#     def my_fun_info(self):
+#         print("name: " + self.name)
+# # 测试1
+# a = Class_F()
+# a.my_fun_info()
+# print(a.name)
+# # 测试2
+# b = Class_F()
+# print(b.name)
+# b.my_fun_info()
+
+# 类属性变化，实例属性会发生变化吗？
+# class Class_G(object):
+#     name = 'huohuazi1016'
+#     def print_name(self):
+#         print('name: ' + self.name)
+
+# A = Class_G()
+# # A.print_name()
+# # Class_G.name='libai'
+# # A.print_name()
+#     # 通过测试，发现类属性改变，实例属性会跟着变化
+
+# # 实例属性改变了，类属性会改变吗？
+# A.name = 'libai'
+# print(Class_G.name)
+#     # 发现，实例属性改变，不会影响类属性
+#     # 不管实例对象怎么修改属性值，对类的属性还是没有影响的。
+
+
+# 实例方法和类方法
+
+    # 结论：类方法改变，实例方法也会跟着改变
+# class Class_H(object):
+#     name = 'huohuazi1016'
+#     def my_info(self):
+#         print('name:' + self.name)
+
+# A = Class_H()
+# A.my_info()
+
+# def my_new_info():
+#     print('火华子1016')
+
+# A.my_info=my_new_info
+# A.my_info()
